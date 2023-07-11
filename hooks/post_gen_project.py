f0e5fca6f4fdd3c83c0ce6a75554c2c89142668a
Biel Stela <biel.stela@gmail.com>
2023-07-11 10:06:56 +0200

    Add .env creation in template and includes it to docker container.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,12 +2,21 @@ import os
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
+ENV_EXAMPLE = """# IMPORTANT: never ever include the .env to git. It contains sensitive information.
+
+API_KEY_EXAMPLE=yourapikeyexample
+"""
 
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
+def create_env_file():
+    with open(os.path.join(PROJECT_DIRECTORY, ".env"), "w") as env_file:
+        env_file.write(ENV_EXAMPLE)
 
 if __name__ == "__main__":
 
     if "Not open source" == "{{ cookiecutter.open_source_license }}":
         remove_file("LICENSE")
+
+    create_env_file()
