f5fd73c5f2334aca16dd991fd40e81e7a5e82a33
Biel Stela <biel.stela@gmail.com>
2023-07-11 10:09:01 +0200

    fix formatting

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,12 +7,15 @@ ENV_EXAMPLE = """# IMPORTANT: never ever include the .env to git. It contains se
 API_KEY_EXAMPLE=yourapikeyexample
 """
 
+
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+
 
 def create_env_file():
     with open(os.path.join(PROJECT_DIRECTORY, ".env"), "w") as env_file:
         env_file.write(ENV_EXAMPLE)
+
 
 if __name__ == "__main__":
 
